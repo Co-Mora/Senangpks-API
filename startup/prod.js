dba17558c73bae28b330a3cbb5caf66c0b1407b0
Omar_Dev <20140354@fa-hists.edu.eg>
2018-10-02 23:06:13 +0800

    Change Routes in quotes

--- startup/prod.js
+++ startup/prod.js
@@ -1,8 +1,9 @@
 const compression = require('compression');
 const helmet = require('helmet');
-
+const cors = require('cors');
 
 module.exports = function (app) {
     app.use(helmet());
     app.use(compression());
+    app.use(cors());
 };

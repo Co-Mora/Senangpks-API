7cb1bb326a42c8cd6a1e76928699ff0667c1d593
Omar_Dev <20140354@fa-hists.edu.eg>
2018-10-02 23:24:08 +0800

    add corss

--- startup/prod.js
+++ startup/prod.js
@@ -3,9 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 module.exports = function (app) {
-    app.use(helmet());
-    app.use(compression());
-    app.use(cors({credentials: true, origin: true}));
-    app.options('*', cors());
+    // app.use(helmet());
+    // app.use(compression());
+    // app.use(cors({credentials: true, origin: true}));
+    // app.options('*', cors());
 
 };

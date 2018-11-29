b3785c0d86508c01340762f0ad655eace27db76a
Omar_Dev <20140354@fa-hists.edu.eg>
2018-11-29 17:48:24 +0800

    Add Routing

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const winston = require('winston');
 const app = express();
 const router = express.Router();
 
+router.get('/api', (req, res) => {
+    res.send("API WORKS");
+});
+
+router.get('/api/v1', (req, res) => {
+    res.send("Im Lost");
+});
+
+
 require('./startup/prod')(app);
 require('./startup/routes')(app);
 require('./startup/logging')();
@@ -11,13 +20,6 @@ require('./startup/db')();
 // const {allowCrossDomain} = require('./middleware/cors');
 // app.use(allowCrossDomain);
 
-router.get('/api', async (req, res) => {
-    res.send("API WORKS");
-});
-
-router.get('/api/v1', async (req, res) => {
-    res.send("Im Lost");
-});
 
 
 const port = process.env.PORT || 3000;
